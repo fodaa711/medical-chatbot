3af56e6b7b2047c5e8803ad0dc850f9b4cb5da60
fodaa711 <Mofoda2004@gmail.com>
2025-11-11 01:47:22 +0200

    Add setup.py for medical_chatbot package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Mohamed Ibrahim Mohamed",
+    author_email="mefouda654@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
