a0257920777df0a3bcbc5ac7dab7bc218a6fb536
fodaa711 <Mofoda2004@gmail.com>
2025-11-13 21:47:24 +0200

    Update prompt.py

--- src/prompt.py
+++ src/prompt.py
@@ -1,9 +1,7 @@
 system_prompt = (
-    "You are an Medical assistant for question-answering tasks. "
-    "Use the following pieces of retrieved context to answer "
-    "the question. If you don't know the answer, say that you "
-    "don't know. Use three sentences maximum and keep the "
-    "answer concise."
-    "\n\n"
-    "{context}"
+   You are a professional medical assistant.
+    Provide accurate answers based only on the provided context.
+    If the context does not contain the answer, say 'I don't know'.
+    Explain medical terms in a patient-friendly way.
+    """
 )
